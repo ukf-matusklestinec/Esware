31dabe7ad270c147966899825c4fa744df27e495
ukf-matusklestinec <61313759+ukf-matusklestinec@users.noreply.github.com>
2022-11-05 14:55:02 +0100

    Update _hero.blade.php
    Tlačidlo "Prihláste sa na Esware sa zobrazí len neprihlásenému používateľovi

--- src/resources/views/partials/_hero.blade.php
+++ src/resources/views/partials/_hero.blade.php
@@ -14,18 +14,16 @@
             Nájdite si prax alebo brigádu cez UKF
         </p>
 
-       {{-- @if() --}}
+       @if(auth()->user() == null)
         <div>
-            {{-- treba ešte pridať možnosti, keď je človek odhlásený --}}
             {{-- v prípade ak používateľ nie je prihlásený, tlačidlo prenesie na prihlasovací formulár --}}
-            <a href="register.html"
+            <a href="/login"
                 class="inline-block border-2 border-white text-white py-2 px-4 rounded-xl uppercase mt-2 hover:text-black hover:border-black"
             >Prihláste sa na EsWare</a>
         </div>
-        {{-- @endif --}}
+        @endif
 
         <div>
-
             {{-- v prípade ak je používateľ prihlásený môže pridať ponuku práce --}}
             <a href="/listings/create"
                 class="inline-block border-2 border-white text-white py-2 px-4 rounded-xl uppercase mt-2 hover:text-black hover:border-black"
