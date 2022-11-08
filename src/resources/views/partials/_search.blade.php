b6bb9a49c177c1c7a85f3d98942b3edaaca1676b
hudorado <58266483+hudorado@users.noreply.github.com>
2022-11-08 09:08:00 +0100

    Uprava search baru

--- src/resources/views/partials/_search.blade.php
+++ src/resources/views/partials/_search.blade.php
@@ -1,14 +1,13 @@
-<form action="/">
-    <div class="relative border-2 border-gray-100 m-4 rounded-lg">
+<form action="/" >
+    <div class="relative border-2 border-black-100 m-4 rounded-lg" style="width: 350px;">
         <div class="absolute top-4 left-3">
-            <i
-                class="fa fa-search text-gray-400 z-20 hover:text-gray-500"
+            <i class="fa fa-search text-gray-400 z-20 hover:text-gray-500"
             ></i>
         </div>
         <input
             type="text"
             name="search"
-            class="h-14 w-full pl-10 pr-20 rounded-lg z-0 focus:shadow focus:outline-none"
+            class="h-14 w-auto pl-10 pr-20 rounded-lg z-0 focus:shadow focus:outline-none"
             placeholder="Hľadať v ponukách..."
         />
         <div class="absolute top-2 right-2">
