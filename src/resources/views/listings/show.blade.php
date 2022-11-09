32a69951425ee0e3eeba8b6e5ba8f72d4fc30bf0
hudorado <58266483+hudorado@users.noreply.github.com>
2022-11-09 20:55:36 +0100

    Detail praxe - drobne upravy

--- src/resources/views/listings/show.blade.php
+++ src/resources/views/listings/show.blade.php
@@ -11,12 +11,15 @@
     .item4 {
         grid-area: footer;
     }
+    .item5 {
+        grid-area: blank;
+    }
 
     .grid-container {
         display: grid;
         grid-template-areas:
-            'photo main main main main main'
-            'photo right right right right right'
+            'photo photo blank main main main'
+            'photo photo blank right right right'
             'footer footer footer footer footer footer';
     }
 
@@ -32,35 +35,37 @@
         <i class="fa-solid fa-arrow-left"></i> Naspäť
     </a>
     <div class="mx-4">
-        <x-card class="p-10 max-w-xl mx-auto mt-6">
+        <x-card class="p-10 max-w-2xl mx-auto mt-6">
             <div class="grid-container">
                 <div class="item1">
                     <img class="w-48 border border-black-200 rounded mb-4" src="{{$listing->logo ? asset('storage/' . $listing->logo) : asset('/images/no-imageUKF.png')}}" alt="" />
                 </div>
                 <div class="item2">
-                    <h3 class="text-3xl mb-2">{{$listing->title}}</h3>
-                    <p class="text-2xl font-bold mb-3">{{$listing->company}}</p>
-                    <div class="text-lg mb-2">
+                    <h3 class="text-2xl mb-2 text-center">{{$listing->title}}</h3>
+                    <p class="text-xl font-bold mb-3">{{$listing->company}}</p>
+                    <div class="text-l">
                         <i class="fa-solid fa-location-dot"></i> {{$listing->location}}
                     </div>
                 </div>
                 <div class="item3">
                     <div>
-                    <a href="{{$listing->website}}" target="_blank" class="text-laravel">
+                        <a href="{{$listing->website}}" target="_blank" class="text-laravel mr-6">
                             <i class="fa-solid fa-globe"></i> Stránka firmy</a>
-                            <br>
+
                         <a href="mailto:{{$listing->email}}" class="text-laravel"><i class="fa-solid fa-envelope"></i>
-                        Kontaktovať</a>
+                            Kontaktovať</a>
                     </div>
 
                 </div>
                 <div class="item4">
-                    <h3 class="text-3xl font-bold mb-4">
+                    <div class="border border-black mb-2"></div>
+                    <h3 class="text-2xl font-bold mb-4">
                         Náplň práce
                     </h3>
-                    <p class="border border-black-200 rounded text-lg space-y-6">{{$listing->description}}</p>
+                    <p class="text-l space-y-6">{{$listing->description}}</p>
                 </div>
             </div>
+            <div class="border border-black mt-2"></div>
             <div class="items-center justify-center text-center flex mt-6 mb-6">
                 <x-listing-tags :tagsCsv="$listing->tags" />
             </div>
@@ -68,9 +73,8 @@
             @unless($aktivity->isEmpty())
 
             @else
-            <a href="/prihlas/{{$listing->id}}" target="_blank" 
-            class="block bg-green-600 text-white py-2 rounded-xl hover:opacity-80 text-center">
-            <i class="fa-solid fa-user"></i> Prihlásiť sa na ponuku</a>
+            <a href="/prihlas/{{$listing->id}}" target="_blank" class="block bg-green-600 text-white py-2 rounded-xl hover:opacity-80 text-center">
+                <i class="fa-solid fa-user"></i> Prihlásiť sa na ponuku</a>
             @endunless
             @endauth
     </div>
