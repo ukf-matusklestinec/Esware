ac8e3e5fa79a38cfcd5549ebd03ffcdc3fc45cc7
ukf-boristoth <61836281+ukf-boristoth@users.noreply.github.com>
2022-12-06 12:48:04 +0100

    drobnosť

--- src/vendor/laravel/framework/src/Illuminate/Pagination/resources/views/bootstrap-5.blade.php
+++ src/vendor/laravel/framework/src/Illuminate/Pagination/resources/views/bootstrap-5.blade.php
@@ -29,13 +29,13 @@
         <div class="d-none flex-sm-fill d-sm-flex align-items-sm-center justify-content-sm-between">
             <div>
                 <p class="small text-muted">
-                    {!! __('Showing') !!}
+                    {!! __('Zobrazených') !!}
                     <span class="fw-semibold">{{ $paginator->firstItem() }}</span>
-                    {!! __('to') !!}
+                    {!! __('až') !!}
                     <span class="fw-semibold">{{ $paginator->lastItem() }}</span>
-                    {!! __('of') !!}
+                    {!! __('z') !!}
                     <span class="fw-semibold">{{ $paginator->total() }}</span>
-                    {!! __('results') !!}
+                    {!! __('výsledkov') !!}
                 </p>
             </div>
 
--- src/vendor/laravel/framework/src/Illuminate/Pagination/resources/views/tailwind.blade.php
+++ src/vendor/laravel/framework/src/Illuminate/Pagination/resources/views/tailwind.blade.php
@@ -25,17 +25,17 @@
         <div class="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
             <div>
                 <p class="text-sm text-gray-700 leading-5">
-                    {!! __('Showing') !!}
+                    {!! __('Zobrazených') !!}
                     @if ($paginator->firstItem())
                         <span class="font-medium">{{ $paginator->firstItem() }}</span>
-                        {!! __('to') !!}
+                        {!! __('až') !!}
                         <span class="font-medium">{{ $paginator->lastItem() }}</span>
                     @else
                         {{ $paginator->count() }}
                     @endif
-                    {!! __('of') !!}
+                    {!! __('z') !!}
                     <span class="font-medium">{{ $paginator->total() }}</span>
-                    {!! __('results') !!}
+                    {!! __('výsledkov') !!}
                 </p>
             </div>
 
